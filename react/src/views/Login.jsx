11ac5f2338b0c41496a786e421d39df388a8869f
purgalo <olasz.g48@gmail.com>
2025-10-29 14:45:22 +0100

    added frontend to login page

--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,7 +1,25 @@
+import { Link } from "react-router-dom";
+
 export default function Login(){
+    
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+    }
+    
     return (
-        <>
-            Login
-        </>
+        <div className="login-signup-form animated fadeInDown">
+            <div className="form">
+                <h1 className="title">Log in to your account</h1>
+                <form onSubmit={onSubmit}>
+                    <input type="email" placeholder="Email"/>
+                    <input type="password" placeholder="Password"/>
+                    <button className="btn btn-block">Login</button>
+                    <p className="message">
+                        Not Registered? <Link to="/signup">Create an account!</Link>
+                    </p>
+                </form>
+            </div>
+        </div>
     )
 }
